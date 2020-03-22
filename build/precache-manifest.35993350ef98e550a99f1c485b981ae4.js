73ccb2810561c777d2b6194b602e3f5d28ae1f65
eshoiza <engrjayt200@gmail.com>
2020-03-22 18:41:52 +0100

    added client-side directories

---
+++ build/precache-manifest.35993350ef98e550a99f1c485b981ae4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058c26ec4a0748b206ed0645083d8575",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c41eb3a44a1d3e79e154",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4f8209321fd6b0b460a2",
+    "url": "/static/js/2.dffd8fa3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.dffd8fa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c41eb3a44a1d3e79e154",
+    "url": "/static/js/main.7d6ce23c.chunk.js"
+  },
+  {
+    "revision": "077c5db229da9032b877",
+    "url": "/static/js/runtime-main.631984d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
