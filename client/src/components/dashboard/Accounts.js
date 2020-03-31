d04de8e2b02f95da67e94675df5d794863d49db1
eshoiza <engrjayt200@gmail.com>
2020-03-31 15:52:32 +0100

    added Account component

--- client/src/components/dashboard/Accounts.js
+++ client/src/components/dashboard/Accounts.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import {connect} from "react-redux";
-import { getAccounts, addAccount} from "../../actions/accountActions";
+import React from "react";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
+import { getAccounts, addAccount } from "../../actions/accountActions";
 import Dashboard from "./Dashboard";
 import PlaidLink from "react-plaid-link";
 
+class Accounts extends React.Component {
+  componentDidMount() {
+    this.props.getAccounts();
+  }
 
- class Accounts extends Component {
-      componentDidMount() {
-          this.props.getAccounts();
-      }
-      
-      //Add
+  // Add account
+  handleOnSuccess = (token, metadata) => {
+    const plaidData = {
+      public_token: token,
+      metadata: metadata
+    };
+    this.props.addAccount(plaidData);
+  };
 
+  // Logout
+  handleLogoutClick = e => {
+    e.preventDefault();
+    this.props.logoutUser();
+  };
 
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
+  render() {
+    const { user } = this.props.auth;
+    const { accounts, accountsLoading } = this.props.plaid;
+
+    let dashboardContent;
+
+    if (accounts == null || accountsLoading) {
+      dashboardContent = <div>Loading...</div>;
+    } else if (accounts.length > 0) {
+      // user has linked accounts
+      dashboardContent = <Dashboard user={user} accounts={accounts} />;
+    } else {
+      // user has no linked accounts
+      dashboardContent = (
+        <div className="plaid-landing">
+          <h3>
+            Welcome, <b>{user.name}</b>
+          </h3>
+          <p>To get started link your first bank account</p>
+          <>
+            <PlaidLink
+              className="plaid"
+              clientName="UNIBANK"
+              env="sandbox"
+              product={["auth", "transactions"]}
+              publicKey={"7d8003fa1ced62bf10e92c084a7ac6"}
+              onExit={this.handleLogoutClick}
+              onSuccess={this.handleOnSuccess}
+              onScriptLoad={() => this.setState({ loaded: true })}
+            >
+              Open Link and connect your bank!
+            </PlaidLink>
+            <button
+              onClick={this.onLogoutClick}
+              className="form-logout-button plaid"
+            >
+              Logout
+            </button>
+          </>
+        </div>
+      );
     }
+    return <div>{dashboardContent}</div>;
+  }
 }
 
+const mapStateToProps = state => ({
+  auth: state.auth,
+  plaid: state.plaid
+});
 
-export default Accounts;
+export default connect(mapStateToProps, {
+  logoutUser,
+  getAccounts,
+  addAccount
+})(Accounts);
---
+++ client/src/components/dashboard/DonutChart.js
@@ -0,0 +1,17 @@
+//import React, { Component } from 'react'
+import React from "react";
+import * as d3 from "d3";
+
+const width = 300;
+const height = 200;
+class DonutChart extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default DonutChart; 
