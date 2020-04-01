dd988338c7c24fb7bc23845a6d5674f9ccd42893
eshoiza <engrjayt200@gmail.com>
2020-04-01 16:23:54 +0100

    added events to register component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,56 +1,109 @@
-import React from 'react';
-import {Link, withRouter} from "react-router-dom";
-import {connect} from  "react-redux";
-import {registerUser} from "../../actions/authActions";
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { registerUser } from "../../actions/authActions";
 
-class Register extends Component {
-    state = {
-        name: "",
-        enail: "",
-        password: "",
-        password2: "",
-        errors: {}
+class Register extends React.Component {
+  state = {
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+    errors: {}
+  };
+
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/accounts");
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
+    }
+  }
+
+  handleOnChange = e => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
+
+  handleOnSubmit = e => {
+    e.preventDefault();
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
     };
-  
-    componentDidMount() {
-        if(this.props.auth.isAuthenticated){
-            this.props.history.push("/accounts");
-        }
-    }
 
-    componentWillReceiveProps(){
-        if(nextProps.errors){
-            this.setState({
-                errors: nextProps.errors
-            });
-        }
-    }
+    this.props.registerUser(newUser, this.props.history);
+  };
 
-    handleOnChange = e=>{
-        this.setState({[e.target.id]: e.target.value});
-    };
+  render() {
+    const { errors } = this.state;
 
-    handleSubmit = e=>{
-        e.preventDefault();
-        const newUser = {
-           name:  this.state.name,
-           email: this.state.email,
-           password: this.state.password,
-           password2: this.state.password2 
-        };
+    return (
+      <div>
+        <nav>
+          <Link className="logo" to="/">
+            <strong>PREMIUM</strong>BANK
+          </Link>
+          <h3>Register</h3>
+          <Link to="/login">Log In</Link>
+        </nav>
 
-        this.props.registerUser(newUser, this.props.history);
-    }
-    
-
-
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+        <form className="form-register" onSubmit={this.handleOnSubmit}>
+          <input
+            onChange={this.handleOnChange}
+            value={this.state.name}
+            error={errors.name}
+            type="text"
+            id="name"
+            placeholder="Name"
+          />
+          <span>{errors.name}</span>
+          <input
+            onChange={this.handleOnChange}
+            value={this.state.email}
+            error={errors.email}
+            type="email"
+            id="email"
+            placeholder="Email"
+          />
+          <span>{errors.email}</span>
+          <input
+            onChange={this.handleOnChange}
+            value={this.state.password}
+            error={errors.password}
+            type="password"
+            id="password"
+            placeholder="Password"
+          />
+          <span>{errors.password}</span>
+          <input
+            onChange={this.handleOnChange}
+            value={this.state.password2}
+            error={errors.password2}
+            type="password"
+            id="password2"
+            placeholder="Confirm password"
+          />
+          <span>{errors.password2}</span>
+          <button className="form-register-button" type="submit">
+            Sign up
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps, { registerUser })(withRouter(Register));
