04dabfa945e06b00b23da644d76bcd4fd52af0a0
eshoiza <engrjayt200@gmail.com>
2020-04-01 15:01:59 +0100

    added Transfer component

---
+++ client/src/components/dashboard/Transfer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Transfer extends Component {
+  state = {
+      account: []
+  };
+
+    render() {
+        return (
+            <div className="wrapper">
+             <h3>Bank Account Transfer</h3>  
+            </div>
+        )
+    }
+}
+export default Transfer; 
