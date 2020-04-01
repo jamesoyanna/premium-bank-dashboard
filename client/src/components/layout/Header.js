9f736388fed7f0c96b73145f9a991893f16da95d
eshoiza <engrjayt200@gmail.com>
2020-04-01 17:16:20 +0100

    added Header component

---
+++ client/src/components/layout/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+     <header className="sub-nav">
+    <h3>Dashboard</h3>
+    <p>You are in the main panel</p>
+    <button
+    className="btn--success modules-button"
+    type="button"
+    name="button"
+    >
+    Add Modules    
+    </button>
+     </header>
+    );
+};
+export default Header;
