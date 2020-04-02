9956c029c955f65248275a8e2655fd875d256a23
eshoiza <engrjayt200@gmail.com>
2020-04-02 20:52:46 +0100

    added functions to handle account submission

---
+++ client/src/components/layout/Panels.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "core-js/stable"
+import "regenerator-runtime/runtime";
+import {connect } from "react-redux";
+import {
+    getTransactions,
+    addAccount,
+    getAuth
+} from "../.."
+class Panels extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default 
