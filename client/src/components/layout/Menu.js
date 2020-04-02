4546a0b21e660f039c6c9fa0cf483549719db582
eshoiza <engrjayt200@gmail.com>
2020-04-02 12:21:31 +0100

    added menu items and icons

--- client/src/components/layout/Menu.js
+++ client/src/components/layout/Menu.js
@@ -8,6 +8,16 @@ const Menu = props => {
                  <li>{props.home}</li> 
                  <i className="fas fa-home active" />
                  <li>{props.savings}</li>
+                 <i className="fas fa-money-bill-alt" />
+                 <li>{props.bills}</li>
+                 <i className="fas fa-credit-card" />
+                 <li>{props.credit}</li>
+                 <i className="fas fa-archive" />
+                 <li>{props.loans}</li>
+                 <i className="fas fa-chart-line"/>
+                 <li>{props.invest}</li>
+                 <i className="fas fa-file-alt" />
+                 <li>{props.contact}</li>
                 
               </ul>
           </aside>
