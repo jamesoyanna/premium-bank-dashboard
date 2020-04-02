505d1ce467d5edf44c46d0e9d1a2fa9c2bb6a35a
eshoiza <engrjayt200@gmail.com>
2020-04-02 12:44:12 +0100

    created Navigation page

---
+++ client/src/components/layout/Navigation.js
@@ -0,0 +1,62 @@
+// import React from 'react';
+// import { Link } from "react-router-dom";
+// import { logoutUser} from 
+// import logout from 
+// import notification from 
+// import profile from 
+
+
+ class Navigation extends Component {
+     state = {
+         query: ""
+     }
+
+     handleSearchTerm = event=>{
+         this.setState({
+             query: event.target.value
+         })
+     };
+    
+     handleLogoutClick = e=>{
+         e.preventDefault();
+         this.props.logoutUser();
+     };
+
+    render() {
+      const { user } = this.props.auth;
+
+        return (
+            <nav>
+                <Link className="logo" to="/">
+                    <strong>PREMIUM</strong>BANK
+                </Link>
+                <input 
+                onChange = {this.handleSearchTerm}
+                type="text"
+                value={this.state.query}
+                placeholder="Search"
+                />
+                <Link className="transfer" to="/transfer">
+                    <button className="btn---default" type="button">
+                        Money Transfer
+                    </button>
+                </Link>
+                <ul className="navbar">
+                    <li className="profile">
+                        <img src={profile} alt="profile face" />
+                            Hello, {user.name}
+                    </li>
+                    <li>
+                        <img src={notification} alt="notification icon" />
+                    </li>
+                    <li className="logout">
+                        
+                    </li>
+                </ul>
+               
+            </nav>
+        )
+    }
+}
+
+export default Navigation;
