1544635a8c1ab0a8f97ce44c2568be1e4ed6cc05
eshoiza <engrjayt200@gmail.com>
2020-04-07 00:47:02 +0100

    checked for token to keep user login

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import {Provider} from "react-redux";
+import jwt_decode from "jwt-decode"
+import setAuthToken from "./utils/setAuthToken";
+import {setCurrentUser, logoutUser} from "./actions/authActions";
+
+import "./css/style.scss";
+import store from "./store";
+
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Account from "./components/dashboard/Accounts";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+
+//Check
+if(localStorage.jwtToken){
+  //se
+}
+
+
 
 function App() {
   return (
